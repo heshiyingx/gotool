38447fe5a0406e71f9b65f822c544d8a9e3b198b
hsy <xxx.@xxx.com>
2024-04-17 16:28:01 +0800

    1

---
+++ gozeroext/lock/lock.go
@@ -0,0 +1,12 @@
+package lock
+
+import (
+	"context"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+)
+
+func GetLock(ctx context.Context, rds *redis.Redis, key string, ttlSec int) *redis.RedisLock {
+	lock := redis.NewRedisLock(rds, "test")
+	lock.SetExpire(ttlSec)
+	return lock
+}
