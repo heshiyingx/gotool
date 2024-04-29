5325a411bce3f290a29791ee078c5377c443e842
hsy <xxx.@xxx.com>
2024-04-29 15:07:49 +0800

    1

--- httpext/requests.go
+++ httpext/requests.go
@@ -3,6 +3,7 @@ package httpext
 import (
 	"context"
 	"encoding/json"
+	"encoding/xml"
 	"github.com/go-resty/resty/v2"
 	"sync"
 )
@@ -37,6 +38,20 @@ func POST[T any](ctx context.Context, url string, param map[string]interface{}, 
 		return result, err
 	}
 	err = json.Unmarshal(response.Body(), &result)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+func POSTXml[T any](ctx context.Context, url string, param map[string]interface{}, header map[string]string) (T, error) {
+	client := clientPool.Get().(*resty.Client)
+	defer clientPool.Put(client)
+	var result T
+	response, err := client.R().SetBody(param).SetHeaders(header).Post(url)
+	if err != nil {
+		return result, err
+	}
+	err = xml.Unmarshal(response.Body(), &result)
 	if err != nil {
 		return result, err
 	}
