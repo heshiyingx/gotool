0810ec18285ec69064903025096ea2bae2fedb5b
hsy <heshiyingx@126.com>
2024-07-31 17:03:14 +0800

    1

--- errext/grpcErrInterceptor.go
+++ errext/grpcErrInterceptor.go
@@ -7,12 +7,12 @@ import (
 )
 
 func CodeErrorInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	// TODO: fill your logic here
 	resp1, err1 := handler(ctx, req)
 	if err1 != nil {
 		logx.WithContext(ctx).Errorf("err: %v,GRPC_FUNC:%v,req:%#v\n", err, info.FullMethod, err, req)
 		return nil, WarpGrpcErr(err1)
 	}
+	logx.WithContext(ctx).Debugf("GRPC_FUNC:%v,req:%#v,resp:%#v\n", info.FullMethod, req, resp1)
 	return resp1, nil
 }
 func CodeErrorSteamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
